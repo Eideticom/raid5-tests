abe1e8985dc43b8aff2b235cca88fd624ff90e8c
Martin Oliveira <martin.oliveira@eideticom.com>
2022-10-12 12:22:39 -0600

    new funccount

--- funccount.py
+++ funccount.py
@@ -12,6 +12,70 @@ class FunccountInvalidFunctionError(Exception):
 class FunccountNotStartedError(Exception):
     pass
 
+
+class FuncCount(threading.Thread):
+    def __init__(self, funcs, sleep=0, executable='funccount-bpfcc'):
+        super().__init__()
+
+        self._funcs = funcs
+
+        if 'bpfcc' in executable:
+            funcs = '|'.join(funcs)
+        else:
+            funcs = ' '.join(funcs)
+
+        self._cmd = [executable, funcs]
+        self._sleep = sleep
+
+        self._stop_event = threading.Event()
+        self._proc = None
+        self._excp = None
+
+    def run(self):
+        if self._stop_event.wait(self._sleep):
+            return
+
+        try:
+            self._proc = sp.Popen(self._cmd, text=True, stdout=sp.PIPE,
+                                  stderr=sp.PIPE, start_new_session=True)
+        except Exception as e:
+            self._excp = e
+
+    def stop(self):
+        self._stop_event.set()
+        #FIXME: do we need this join? Probably not
+        #self.join()
+
+        if self._excp:
+            raise self._excp
+
+        if not self._proc:
+            return
+
+        pgid = os.getpgid(self._proc.pid)
+        os.killpg(pgid, signal.SIGINT)
+
+        try:
+            out, err = self._proc.communicate(timeout=1)
+        except sp.TimeoutExpired:
+            self._proc.kill()
+            out, err = self._proc.communicate()
+
+        if self._proc.returncode:
+            raise sp.CalledProcessError(cmd=self._cmd,
+                                        returncode=self._proc.returncode,
+                                        output=out,
+                                        stderr=err)
+
+        res = {}
+        print(out.split('\n'))
+        for line in out.split('\n'):
+            line = line.strip().split()
+            if len(line) > 0 and line[0] in self._funcs:
+                res[line[0]] = int(line[1])
+
+        return res
+
 class Funccount:
     def _sleep_and_run(self):
         self.event.wait(self.sleep)
